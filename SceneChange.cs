afdd12b05a7945ea21d0e5db7b3a9aca1b694786
artyomp1506 <84268823+artyomp1506@users.noreply.github.com>
2021-06-12 14:45:20 +0500

    Update SceneChange.cs

--- SceneChange.cs
+++ SceneChange.cs
@@ -5,17 +5,6 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 public class SceneChange : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
     
     public void ChangeScene(int number)
     {
