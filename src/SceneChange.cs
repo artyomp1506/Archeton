62af188781a2e661595f64091075e3a9d1b968d4
artyomp1506 <84268823+artyomp1506@users.noreply.github.com>
2021-06-12 15:56:00 +0500

    Перемещение скрипта в папку src

---
+++ src/SceneChange.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class SceneChange : MonoBehaviour
+{
+    
+    public void ChangeScene(int number)
+    {
+        SceneManager.LoadScene(number);
+    }
+}
