68f332a085627d6f0a5907368f7e2a02698c9ee9
artyomp1506 <84268823+artyomp1506@users.noreply.github.com>
2021-06-16 10:58:55 +0500

    Upload Unity

---
+++ Material/Unity/Assets/Scenes/Scripts/CheckAnswer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckAnswer : MonoBehaviour
+{
+    public int Right, Wrong;
+    public GameObject ScoreText;
+
+
+    public void CountRight()
+    {
+        Right++;
+        ScoreText.GetComponent<Text>().text = Right.ToString();
+    }
+    public void CountWrong()
+    {
+        Wrong++;
+        ScoreText.GetComponent<Text>().text = Wrong.ToString();
+
+    }
+
+}
